ffcfb7776c6d4fd1810b1834ddd4bc1eac6c08ef
Mike Steele <mike@steel.fm>
2019-10-31 00:55:33 -0400

    Add example CSS module

--- mike-dot-cool/src/App.js
+++ mike-dot-cool/src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Section from './Section';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <Section />
   );
 }
 
---
+++ mike-dot-cool/src/Section.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import s from './Section.module.css';
+
+const Section = () => <div className={s.Wrapper}>:-)</div>
+
+export default Section;
