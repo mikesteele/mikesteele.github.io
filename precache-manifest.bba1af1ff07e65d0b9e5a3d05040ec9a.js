e6a69f8ed8b8078ee7c0a5de897678793bf21ecb
Mike Steele <mike@steel.fm>
2019-10-31 00:34:25 -0400

    Updates

---
+++ precache-manifest.bba1af1ff07e65d0b9e5a3d05040ec9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf3ea7b8bc1b5882b27f58f1bc1acf7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a1b2112a22c388b83db",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2ce9e8ff9c281d666422",
+    "url": "/static/js/2.49be5bf2.chunk.js"
+  },
+  {
+    "revision": "6a1b2112a22c388b83db",
+    "url": "/static/js/main.784b9fe4.chunk.js"
+  },
+  {
+    "revision": "94ca428852b41b2cfb6e",
+    "url": "/static/js/runtime-main.e2a58eec.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
