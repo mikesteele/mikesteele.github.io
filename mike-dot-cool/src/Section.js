1b646978be338a5b04a0ebeabe37a25c8e20440f
Mike Steele <mike@steel.fm>
2019-11-01 01:05:25 -0400

    Minor change

--- mike-dot-cool/src/Section.js
+++ mike-dot-cool/src/Section.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import s from './Section.module.css';
 
-const Section = () => <div className={s.Wrapper}>:-)</div>
+const Section = () => <div className={s.Wrapper}>:-) wow!</div>
 
 export default Section;
