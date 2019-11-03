b60a79eed3fa07d8df5a0465bfafc314f8fffe84
Mike Steele <mike@steel.fm>
2019-11-03 11:48:45 -0500

    Initial commit

--- mike-dot-cool-v2/src/Image.js
+++ mike-dot-cool-v2/src/Image.js
@@ -1,14 +1,10 @@
-import ReactDOM from 'react-dom'
 import * as THREE from 'three/src/Three'
-import React, { useState, useRef, useEffect, useCallback, useMemo } from 'react'
-import { apply as applyThree, Canvas, useRender, useThree } from 'react-three-fiber'
+import React, { useMemo } from 'react'
 
-/** This component loads an image and projects it onto a plane */
 function Image({ url, opacity, scale, ...props }) {
-  const texture = useMemo(() => new THREE.CanvasTexture( document.querySelector('#myCanvas') ), [url])
-  //const texture = useMemo(() => new THREE.TextureLoader().load(url), [url])
+  const texture = useMemo(() => new THREE.TextureLoader().load(url), [url])
   return (
-    <meshLambertMaterial attach="material" opacity={0.3}>
+    <meshLambertMaterial attach="material" >
       <primitive attach="map" object={texture} />
     </meshLambertMaterial>
   )
--- mike-dot-cool-v2/src/index.js
+++ mike-dot-cool-v2/src/index.js
@@ -1,192 +1,68 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef } from 'react'
 import ReactDOM from 'react-dom'
 import { Canvas, useFrame } from 'react-three-fiber'
 import './index.css';
 import Image from './Image';
-import useInterval from './useInterval';
-import {Spring} from 'react-spring/renderprops'
+import { Spring } from 'react-spring/renderprops'
 
-
-/**
-const useSpring = (toValue, oldValue, frameCount) => {
-  const lastValue = useRef();
-  const springAmount = Math.abs((toValue - oldValue)) / frameCount;
-  useFrame(() => {
-    if (lastValue.current) {
-      if (lastValue.current === toValue) {
-        // No-op
-      } else if (Math.abs(lastValue.current - toValue) <= springAmount) {
-        lastValue.current = toValue
-      } else if (lastValue.current < toValue) {
-        lastValue.current = lastValue.current + springAmount
-      } else if (lastValue.current > toValue) {
-        lastValue.current = lastValue.current - springAmount
-      }
-    } else {
-      lastValue.current = oldValue;
-    }
-  });
-  return lastValue.current;
-}
-*/
-
-const useSpringYPosition = (ref, toValue, springAmount) => {
-  useFrame(() => {
-    if (ref.current) {
-      if (ref.current.position.y === toValue) {
-        return
-      } else if (Math.abs(ref.current.position.y - toValue) <= springAmount) {
-        ref.current.position.y = toValue
-      } else if (ref.current.position.y < toValue) {
-        ref.current.position.y = ref.current.position.y + springAmount
-      } else if (ref.current.position.y > toValue) {
-        ref.current.position.y = ref.current.position.y - springAmount
-      }
-    }
-  })
-}
-
-const useSpringXScale = (ref, toValue, springAmount) => {
-  useFrame(() => {
-    if (ref.current) {
-      if (ref.current.scale.x === toValue) {
-        return
-      } else if (Math.abs(ref.current.scale.x - toValue) <= springAmount) {
-        ref.current.scale.x = toValue
-      } else if (ref.current.scale.x < toValue) {
-        ref.current.scale.x = ref.current.scale.x + springAmount
-      } else if (ref.current.scale.x > toValue) {
-        ref.current.scale.x = ref.current.scale.x - springAmount
-      }
-    }
-  })
-}
-
-const useSpringYScale = (ref, toValue, springAmount) => {
-  useFrame(() => {
-    if (ref.current) {
-      if (ref.current.scale.y === toValue) {
-        return
-      } else if (Math.abs(ref.current.scale.y - toValue) <= springAmount) {
-        ref.current.scale.y = toValue
-      } else if (ref.current.scale.y < toValue) {
-        ref.current.scale.y = ref.current.scale.y + springAmount
-      } else if (ref.current.scale.y > toValue) {
-        ref.current.scale.y = ref.current.scale.y - springAmount
-      }
-    }
-  })
-}
-
-
-const useSpringXRotation = (ref, toValue, springAmount) => {
-  useFrame(() => {
-    if (ref.current) {
-      if (ref.current.rotation.x === toValue) {
-        return
-      } else if (Math.abs(ref.current.rotation.x - toValue) <= springAmount) {
-        ref.current.rotation.x = toValue
-      } else if (ref.current.rotation.x < toValue) {
-        ref.current.rotation.x = ref.current.rotation.x + springAmount
-      } else if (ref.current.rotation.x > toValue) {
-        ref.current.rotation.x = ref.current.rotation.x - springAmount
-      }
-    }
-  })
-}
-
-const useSpringZRotation = (ref, toValue, springAmount) => {
-  useFrame(() => {
-    if (ref.current) {
-      if (ref.current.rotation.cube1ZRotation === toValue) {
-        return
-      } else if (Math.abs(ref.current.rotation.z - toValue) <= springAmount) {
-        ref.current.rotation.z = toValue
-      } else if (ref.current.rotation.z < toValue) {
-        ref.current.rotation.z = ref.current.rotation.z + springAmount
-      } else if (ref.current.rotation.z > toValue) {
-        ref.current.rotation.z = ref.current.rotation.z - springAmount
-      }
-    }
-  })
-}
-function Thing(props) {
+const MorphCube = props => {
   const { previousShape, shape } = props
   const cube1Ref = useRef()
-  const cube1YPosition = {
-    none: 0,
-    cube: 0,
-    dc: 0,
-    resume: 0
-  }
 
-  const cube1XScale = {
-    none: 1,
-    cube: 1,
-    dc: 0.01,
-    resume: 0
-  }
-
-  const cube1YScale = {
-    none: 1,
-    cube: 1,
-    dc: 1,
-    resume: 1
-  }
-
-  const cube1XRotation = {
-    none: 0,
-    cube: 0,
-    dc: 0,
-    resume: 0
-  }
-
-
-  const springAmountForFrameCount = (oldValue, newValue, frameCount) => {
-    return Math.abs((newValue - oldValue)) / frameCount
-  }
   useFrame(() => {
     if (shape === 'cube') {
-
       cube1Ref.current.rotation.x = cube1Ref.current.rotation.x + 0.01
+      cube1Ref.current.rotation.y = cube1Ref.current.rotation.y + 0.01
     }
-          cube1Ref.current.rotation.y = cube1Ref.current.rotation.y + 0.01
   });
 
-  const color =  'white';
+  const shapes = {
+    cube: {
+      xScale: 1,
+      zScale: 1,
+      zPosition: 0,
+      xRotation: 1
+    },
+    dc: {
+      xScale: 1.6,
+      zScale: 0.1,
+      zPosition: 1,
+      xRotation: 0
+    },
+    none: {
+      xScale: 0.01,
+      zScale: 0.01,
+      zPosition: -10,
+      xRotation: -1
+    }
+  }
+
   return (
     <Spring
       from={{
-        xScale: shape === 'cube' ? 4 : 1,
-        zScale: shape === 'cube' ? 0.1 : 1,
-        zPosition: shape === 'cube' ? -4 : 0,
-        xRotation:  shape === 'cube' ? 0 : 0.5
+        xScale: shapes[previousShape].xScale,
+        zScale: shapes[previousShape].zScale,
+        zPosition: shapes[previousShape].zPosition,
+        xRotation: shapes[previousShape].xRotation
       }}
       to={{
-        xScale: shape === 'cube' ? 1 : 4,
-        zScale: shape === 'cube' ? 1 : 0.1,
-        zPosition: shape === 'cube' ? 0 : -4,
-        xRotation:  shape === 'cube' ? 0.5 : 0
+        xScale: shapes[shape].xScale,
+        zScale: shapes[shape].zScale,
+        zPosition: shapes[shape].zPosition,
+        xRotation: shapes[shape].xRotation
       }}
     >
-      {props => {
-        const extraProps = {};
-        if (shape !== 'cube') {
-          extraProps.rotation = [props.xRotation, 0, 0];
-        }
-        return (
-          <mesh ref={cube1Ref} position={[0,0,props.zPosition]} rotation={shape === 'cube' ? [0,0,0]: [props.xRotation, 0, 0]}>
-            <boxBufferGeometry attach="geometry" args={[props.xScale, 1, props.zScale]} />
-            {shape === 'cube' ? (
-              <meshPhongMaterial color={color} wireframe attach="material" />
-            ): (
-              <Image url="/1.png" />
-            )}
-            <boxBufferGeometry attach="geometry" args={[props.xScale, 1, props.zScale]} />
-
-          </mesh>
-        )
-      }}
+      {props => (
+        <mesh ref={cube1Ref} position={[0,0,props.zPosition]} rotation={[props.xRotation, 0, 0]}>
+          <boxBufferGeometry attach="geometry" args={[props.xScale, 1, props.zScale]} />
+          {shape === 'cube' ? (
+            <meshNormalMaterial color="white" wireframe attach="material" />
+          ): (
+            <Image url="/dc.png" />
+          )}
+          <boxBufferGeometry attach="geometry" args={[props.xScale, 1, props.zScale]} />
+        </mesh>
+      )}
       </Spring>
   )
 }
@@ -208,30 +84,17 @@ const App = () => {
       shape: 'cube'
     }));
   }
-  const onMouseOverResume = e => {
-    setShapes(shapes => ({
-      previousShape: shapes.shape,
-      shape: 'resume'
-    }));
-  }
-  const onMouseOutResume = e => {
-    setShapes(shapes => ({
-      previousShape: shapes.shape,
-      shape: 'cube'
-    }));
-  }
   return (
     <>
-  <a href="https://github.com/mikesteele/dual-captions">
-     <div className='links' onMouseOver={onMouseOverDC} onMouseOut={onMouseOutDC}>
-        dual-captions
-      </div>
+      <a href="https://github.com/mikesteele/dual-captions">
+       <div className='links' onMouseOver={onMouseOverDC} onMouseOut={onMouseOutDC}>
+          dual-captions
+        </div>
       </a>
-
       <Canvas camera={{ position: [0, 0, 2] }}>
         <ambientLight intensity={0.5} />
         <spotLight intensity={0.6} position={[30, 30, 50]} angle={0.2} penumbra={1} castShadow />
-        <Thing shape={shapes.shape} previousShape={shapes.previousShape} />
+        <MorphCube shape={shapes.shape} previousShape={shapes.previousShape} />
       </Canvas>
     </>
   )
